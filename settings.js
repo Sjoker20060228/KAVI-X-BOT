7ae26164a6c4d56794aff19e6ca9d11d2932105f
Lahiru <paneljoker2000@gmail.com>
2025-07-01 19:28:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~4UBTkL5b#KxB7BiRSO8OMwg-7mhgTKq5nxfMjrbhgs5NNjAsMpvY"
-global.botname = process.env.BOTNAME || "polo"
+global.botname = process.env.BOTNAME || "𝔻𝕩𝔾 ℙ𝕆𝕃𝕆 "
+
 global.ownernumber = process.env.OWNERNUMBER |0721977999| ""
 global.ownername = process.env.OWNERNAME || "DxG POLO "
 global.websitex = process.env.WEBSITEX || "https://kavi-x-bot-login.up.railway.app/"
